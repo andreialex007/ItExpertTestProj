34e5a1d5feaed6d058262ca08b1480eb475c3407
Andrei <andreialexandrovich@gmail.com>
2024-05-10 23:42:04 +0200

    db

---
+++ TaskFirst/WebApp/Data/Item.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApp.Data;
+
+public class Item
+{
+    public int Id { get; set; }
+    public int Code { get; set; }
+    public string Value { get; set; }
+}
--- TaskFirst/WebApp/Program.cs
+++ TaskFirst/WebApp/Program.cs
@@ -1,10 +1,15 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WebApp.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers(); 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -17,6 +22,22 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+using (var scope = app.Services.GetService<IServiceScopeFactory>().CreateScope())
+{
+    using var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    var databaseFacade = db.Database;
+    databaseFacade.SetConnectionString(connectionString);
+    databaseFacade.Migrate();
+    db.Items.Add(new Item
+    {
+        Code = 5,
+        Value = "test"
+    });
+    //db.SaveChanges();
+}
+
 
 app.MapControllers();
 
