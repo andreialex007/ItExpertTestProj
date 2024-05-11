152c87e8836e9ba0f76cd9714feb6b67f8311d56
Andrei <andreialexandrovich@gmail.com>
2024-05-11 18:11:09 +0200

    fix

--- TaskFirst/WebApp/Data/Item.cs
+++ TaskFirst/WebApp/Data/Item.cs
@@ -1,8 +1,13 @@
-﻿namespace WebApp.Data;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 
+namespace WebApp.Data;
+
+[Index(nameof(Code), nameof(Value), Name = "idx_code_value")]
 public class Item
 {
     public int Id { get; set; }
     public int Code { get; set; }
-    public string Value { get; set; }
+
+    [StringLength(255)] public string Value { get; set; }
 }
---
+++ TaskFirst/WebApp/Migrations/20240511160932_AddCompositeIndex.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCompositeIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Value",
+                table: "Items",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "idx_code_value",
+                table: "Items",
+                columns: new[] { "Code", "Value" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "idx_code_value",
+                table: "Items");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Value",
+                table: "Items",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255,
+                oldNullable: true);
+        }
+    }
+}
